c1ec3efac1ada2b4d2453787787093c8e1cc4b93
DevBikini <110376121+DevBikini@users.noreply.github.com>
2022-08-15 21:45:55 +0800

    Update @.js

--- commands/@.js
+++ commands/@.js
@@ -153,7 +153,7 @@ function Getko(users, amount, recent) {
   if (user == 1) {
     return amount
   } else {
-    var random = Libs.Random.randomFloat(1, recent)
+    var random = Math.floor(Math.random() * (recent - 1))
     var clc = (amount * random) / 100
     var dam = clc + "&" + random
     return dam

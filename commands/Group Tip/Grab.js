924992b36ec4d17897ad1bca3e596794588dd2c7
DevBikini <110376121+DevBikini@users.noreply.github.com>
2022-08-15 17:59:59 +0800

    Update Grab.js

--- commands/Group Tip/Grab.js
+++ commands/Group Tip/Grab.js
@@ -14,22 +14,99 @@ if (request.sender_chat && request.sender_chat.type == "channel") {
   return
 }
 var grab = Bot.getProperty("grab=" + request.chat.id, { list: {} })
-var draw = Bot.getProperty("draw_" + request.chat.id, { list: {} })
+var draw = Bot.getProperty("draw=" + request.chat.id, { list: {} })
 var data = Bot.getProperty(user.telegramid)
 if (!params) {
   //draw
   if (draw) {
-    //1
     for (var ind in draw.list) {
-      //2
+      var id = draw.list[ind].id
+      var addko = Bot.getProperty("list2_" + id, { list: {} })
+      var numberPeople = draw.list[ind].numberPeople
+      var participate = draw.list[ind].participate
+      var currency = draw.list[ind].currency
+      var balance = Libs.ResourcesLib.userRes(currency)
+      var owner = draw.list[ind].owner
       var amount = draw.list[ind].amount
-      if (amount) {
-        Bot.inspect(draw.list[ind])
+      var json = {
+        id: id,
+        amount: amount,
+        numberPeople: numberPeople,
+        participate: participate + 1,
+        currency: currency,
+        owner: owner
       }
-      //2
+      if (participate + 1 < numberPeople) {
+        if (!addko.list[user.telegramid]) {
+          addko.list[user.telegramid] = user.telegramid
+          Bot.setProperty("list2_" + id, addko, "json")
+          draw.list[id] = json
+          Bot.setProperty("draw=" + request.chat.id, draw, "json")
+          Api.sendMessage({
+            text:
+              "<b>" +
+              currency +
+              "</b> <code>+" +
+              amount +
+              "</code> " +
+              data.html,
+            parse_mode: "html"
+          })
+          balance.add(+amount)
+        }
+      }
+      //next
+      if (participate < numberPeople) {
+        if (!addko.list[user.telegramid]) {
+          Api.sendMessage({
+            text:
+              "<b>" +
+              currency +
+              "</b> <code>+" +
+              amount +
+              "</code> " +
+              data.html,
+            parse_mode: "html"
+          })
+          balance.add(+amount)
+
+          var addG = Bot.getProperty("list2_" + id, { list: {} })
+          addG.list[user.telegramid] = user.telegramid
+          Bot.setProperty("list2_" + id, addG, "json")
+          draw.list[id] = Bot.setProperty(
+            "draw=" + request.chat.id,
+            draw,
+            "json"
+          )
+          var winner = ""
+          var no = 0
+          for (var ind in addG.list) {
+            var no = no + 1
+            var winner =
+              winner +
+              no +
+              ". " +
+              Bot.getProperty(addG.list[ind]).html +
+              " <code>" +
+              amount +
+              "</code> <b>" +
+              currency +
+              "</b>\n"
+          }
+          Api.sendMessage({
+            text:
+              Bot.getProperty(owner).html +
+              " <code>" +
+              numberPeople +
+              "</code> users collected your draw.\n\n" +
+              winner,
+            parse_mode: "html"
+          })
+        }
+      }
     }
-    //1
   }
+  //Grab/airdrop
   if (grab) {
     //airdrop
     for (var ind in grab.list) {
@@ -221,4 +298,3 @@ if (!params) {
     }
   }
 }
-
